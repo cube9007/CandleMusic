54fcfe9809e711641450b1718e73557d9bef9326
최원호 <cube9007@naver.com>
2025-02-01 23:04:45 +0900

    defaultSetting2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
-import { fileURLToPath } from 'url'
-
-// Convert import.meta.url to a file path
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
