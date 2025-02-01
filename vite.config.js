6d2f115422f7eb89eb4c85b9fcc2dd28099ca48a
최원호 <cube9007@naver.com>
2025-02-01 22:56:22 +0900

    github page commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
+import { fileURLToPath } from 'url'
+
+// Convert import.meta.url to a file path
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +14,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
+  base: "/CandleMusic/",
   plugins: [react()],
 })
